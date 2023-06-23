1d8be427457059e448ac9ff2e591c2f4533e660f
Oğuzhan Doğan <81444819+OguzhanDogan0@users.noreply.github.com>
2023-06-24 01:47:28 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,20 +10,35 @@ systemctl reload sshd
 echo "Set root password"
 echo -e "admin\nadmin" | passwd root >/dev/null 2>&1
 
-#JenkinsInstall
-sudo apt install -qq openjdk-11-jdk -y  >/dev/null 2>&1
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update -qq >/dev/null 2>&1
-sudo apt install -qq jenkins -y >/dev/null 2>&1
-sudo systemctl enable --now jenkins
+# This is the Debian package repository of Jenkins to automate installation and upgrade. 
+# To use this repository, first add the key to your system:
+echo "Add Debian package"
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
-#DockerInsall
+# Then add a Jenkins apt repository entry:
+echo "add a Jenkins apt repository entry"
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
 sudo apt-get update -qq >/dev/null 2>&1
-sudo apt-get install ca-certificates curl gnupg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# install java
+echo "install openjdk-11-jre"
+sudo apt-get install -qq -y fontconfig openjdk-11-jre >/dev/null 2>&1
+
+# install Jenkins
+echo "install Jenkins"
+sudo apt-get install -qq -y jenkins  >/dev/null 2>&1
+
+# docker install
+echo "install docker"
 sudo apt-get update -qq >/dev/null 2>&1
-sudo apt-get install -qq -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin >/dev/null 2>&1
+sudo apt install -qq -y docker.io >/dev/null 2>&1
+
+# start docker 
+echo "start docker"
+sudo systemctl enable --now docker
+
+#install nodejs and npm
+echo "install nodejs and npm"
+sudo apt install -qq -y nodejs >/dev/null 2>&1
+sudo apt install -qq -y npm >/dev/null 2>&1
